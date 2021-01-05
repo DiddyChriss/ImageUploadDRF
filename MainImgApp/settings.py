fae4868bab0f2ddd9a727cfcc65343798472c881
Krzysztof <63428054+DiddyChriss@users.noreply.github.com>
2021-01-05 19:19:19 +0100

    Update settings.py

--- MainImgApp/settings.py
+++ MainImgApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8h=*k%nr0g^av+o$uo$c@56buzv2r47a4og^ze16&up#5a*cp3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
